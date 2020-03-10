beee6020f5a7d4db15cd916bcfca4b93ca8a4d46
Lavesh Gaurav <39463503+laveshgaurav@users.noreply.github.com>
2020-03-11 03:48:38 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import Navbar from './layouts/Navbar';
+import Footer from './layouts/Footer';
+import Register from './components/Register';
+import Home from './components/Home';
+import Courses from './components/modals';
+import Alert_Success from './components/Alert_Success';
+import Alert_Fail from './components/Alert_Fail';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      success: 'Registration will start soon'
+    };
+
+    this.onSubmit = fields => {
+      this.setState({
+        success: fields
+      });
+    };
+  }
+
+  render() {
+    var showAlert;
+    const message = this.state.success;
+    if (this.state.success == true) {
+      showAlert = <Alert_Success />;
+    } else {
+      showAlert = <Alert_Fail message={message} />;
+    }
+
+    return (
+      <BrowserRouter>
+        <div className='App'>
+          <Navbar></Navbar>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+              <Courses />
+            </Route>
+            <Route exact path='/register'>
+              <br />
+              {showAlert}
+              <Register onSubmit={fields => this.onSubmit(fields)} />
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import './home.css';
+import { gsap } from 'gsap';
+import Logo from '../assets/Elabs logo black.png';
+import { Link } from 'react-router-dom';
+function Home() {
+  useEffect(() => {
+    if (window.innerWidth >= 768) {
+      gsap.from('.one', { attr: { flex: 0 }, x: -1000, duration: 1.2 });
+      gsap.from('.two', { attr: { flex: 5 }, duration: 1.2, delay: 1.6 });
+      gsap.from('.overflow', { y: -1000, duration: 1.2, delay: 2.4 });
+      gsap.from('.h6', { y: -100, duration: 1.2, delay: 2.4, opacity: 0 });
+      gsap.from('.h1', { y: -100, duration: 1.2, delay: 3.6, opacity: 0 });
+      gsap.from('button', { x: -400, duration: 1.2, delay: 3.6, opacity: 0 });
+    }
+  });
+  return (
+    <div>
+      <div className='wrapper'>
+        <div className='one'>
+          <div className='heading'>
+            <h6 className='h6'>Become a part of</h6>
+            <h1 className='h1'>E LABS</h1>
+            <button>
+              <Link to='/register'>REGISTER</Link>
+            </button>
+          </div>
+        </div>
+        <div className='two'>
+          <div className='overflow'>
+            <img src={Logo}></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
